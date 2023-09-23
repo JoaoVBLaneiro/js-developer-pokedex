7bd5d8472a5bff885d76a0f14b235b60233e414d
JVBL <52715584+JoaoVBLaneiro@users.noreply.github.com>
2023-09-23 11:32:30 -0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,47 +1,52 @@
-const pokemonList = document.getElementById('pokemonList')
-const loadMoreButton = document.getElementById('loadMoreButton')
 
-const maxRecords = 151
-const limit = 10
+const pokemonList = document.getElementById('pokemonList');
+const loadMoreButton = document.getElementById('loadMoreButton');
+
+const maxRecords = 151;
+const limit = 8;
 let offset = 0;
 
-function convertPokemonToLi(pokemon) {
-    return `
-        <li class="pokemon ${pokemon.type}">
-            <span class="number">#${pokemon.number}</span>
-            <span class="name">${pokemon.name}</span>
-
-            <div class="detail">
-                <ol class="types">
-                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
-                </ol>
-
-                <img src="${pokemon.photo}"
-                     alt="${pokemon.name}">
-            </div>
-        </li>
-    `
+function verDetalhes(pokemonId){
+    window.location.href = `detail.html?id=${pokemonId}` 
 }
 
-function loadPokemonItens(offset, limit) {
+function loadPokemonItems(offset, limit){
     pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
-        const newHtml = pokemons.map(convertPokemonToLi).join('')
-        pokemonList.innerHTML += newHtml
+        const newHtml = pokemons.map((pokemon) => `
+            <li class="pokemon ${pokemon.type}">
+                <span class="number">#${pokemon.number}</span>
+                <span class="name">${pokemon.name}</span>
+                        
+                <div class="detail">
+                    <ol class="types">
+                        ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                    </ol>
+        
+                    <img src="${pokemon.photo}"
+                        alt="${pokemon.name}">
+
+                    <button class="button ${pokemon.type} id="detail_btn" onclick="verDetalhes(${pokemon.number})">...</button>
+                </div>
+
+            </li>
+        `).join('');
+        pokemonList.innerHTML += newHtml;
     })
 }
 
-loadPokemonItens(offset, limit)
+loadPokemonItems(offset, limit)
 
 loadMoreButton.addEventListener('click', () => {
     offset += limit
-    const qtdRecordsWithNexPage = offset + limit
 
-    if (qtdRecordsWithNexPage >= maxRecords) {
-        const newLimit = maxRecords - offset
-        loadPokemonItens(offset, newLimit)
+    const qtdRecordWithNextPage = offset + limit;
 
-        loadMoreButton.parentElement.removeChild(loadMoreButton)
+    if(qtdRecordWithNextPage >= maxRecords){
+        const newLimit = maxRecords - offset;
+        loadPokemonItems(offset, newLimit)
+
+        loadMoreButton.parentElement.removeChild(loadMoreButton);
     } else {
-        loadPokemonItens(offset, limit)
-    }
+        loadPokemonItems(offset, limit)
+    }  
 })
