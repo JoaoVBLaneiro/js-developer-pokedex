15e89ea92db24193239b66de9bf0fb3e2ae7f516
JVBL <52715584+JoaoVBLaneiro@users.noreply.github.com>
2023-09-23 11:33:23 -0300

    Update pokemon-model.js

--- assets/js/pokemon-model.js
+++ assets/js/pokemon-model.js
@@ -6,3 +6,15 @@ class Pokemon {
     types = [];
     photo;
 }
+
+class PokeStats extends Pokemon{
+    hp;
+    atk;
+    def;
+    sp_atk;
+    sp_def;
+    speed;
+    height;
+    weight;
+    abilities = [];
+}
