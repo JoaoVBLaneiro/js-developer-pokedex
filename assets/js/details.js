d9a973588fc7ccb7b8885a8f6a75dc338fa08cc2
JVBL <52715584+JoaoVBLaneiro@users.noreply.github.com>
2023-09-24 09:45:09 -0300

    Update details.js

--- assets/js/details.js
+++ assets/js/details.js
@@ -9,6 +9,14 @@ function exibirDetalhesPokemon() {
         .then((jsonBody) => DetailsPokePage(jsonBody))
         .catch((error) => console.log(error));
 }
+
+function pad(number, length) {
+  var str = '' + number;
+  while (str.length < length) {
+      str = '0' + str;
+  }
+  return str;
+}
     
 function DetailsPokePage(detailsPoke) {
     
@@ -48,6 +56,28 @@ function exibirDetalhes(pokemon) {
     
     const detalhes = document.createElement("div");
     detalhes.classList.add("detalhe");
+
+    const poke_num_pad = pad(pokemon.number, 3);
+    
+    var proximo_num = pokemon.number + 1;
+    var proximo_num_pad = 0;
+    if(proximo_num > 151){
+      proximo_num = 1;
+      proximo_num_pad = pad(proximo_num, 3);
+    }else{
+      proximo_num_pad = pad(proximo_num, 3);
+    }
+    
+    var num_anterior = pokemon.number - 1;
+    var num_anterior_pad = 0;
+    if(num_anterior < 1){
+      num_anterior = 151;
+      num_anterior_pad = pad(num_anterior, 3);
+    }else{
+      num_anterior_pad = pad(num_anterior, 3);
+    }
+
+
     
     detalhes.innerHTML = `
       <div class="card ${pokemon.type}">
@@ -63,7 +93,7 @@ function exibirDetalhes(pokemon) {
               
               <div class="name_number">
                 <h1>${pokemon.name}</h1>
-                  <h3>#${pokemon.number}</h3>
+                  <h3>#${poke_num_pad}</h3>
               </div>
                   
               <div class="tags">
@@ -79,14 +109,14 @@ function exibirDetalhes(pokemon) {
         <div class="card_body">
           
         <div class="tab">
-            <button class="tablinks" onclick="openCity(event, 'about')">About</button>
+            <button class="tablinks" onclick="openCity(event, 'about')">Sobre</button>
             <button class="tablinks" onclick="openCity(event, 'base')">Base Stats</button>
           </div>
           
             <div id="about" class="tabcontent">
-              <ol class="ol_about"><p>Height</p><li>${pokemon.height} m</li></ol>
-              <ol class="ol_about"><p>Weight</p><li>${pokemon.weight} Kg</li></ol>
-              <ol class="ol_about"><p>Abilities</p>${pokemon.abilities.map((ability) => `<li>${ability}</li>`)
+              <ol class="ol_about"><p>Altura</p><li>${pokemon.height} m</li></ol>
+              <ol class="ol_about"><p>Peso</p><li>${pokemon.weight} Kg</li></ol>
+              <ol class="ol_about"><p>Habilidades</p>${pokemon.abilities.map((ability) => `<li>${ability}</li>`)
               .join("")}
               </ol>                    
             </div>
@@ -153,7 +183,17 @@ function exibirDetalhes(pokemon) {
                   </ol>
                 </div>
           </div>
-        </div>           
+        </div>
+        
+        <section class="icons">
+                  <div class="left_arrow">
+                    <a href="detail.html?id=${num_anterior}"><span>&#8592 #${num_anterior_pad}</span></a>
+                  </div>                    
+
+                <div class="right_arrow">
+                  <a href="detail.html?id=${proximo_num}"><span>#${proximo_num_pad} &#8594;</span></a>
+                </div>                    
+              </section> 
           
           `;
           detailsContainer.appendChild(detalhes);
